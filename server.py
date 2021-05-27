b0e4764e5ee58413f0a14cb04150cd9bac542982
antonz <tenundor@gmail.com>
2021-05-27 13:40:58 +0300

    Add simple login logic

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
 app = FastAPI()
+
+users = {
+    'tenundor@gmail.com': {
+        'password': '12345',
+        'balance': 10_000,
+    }
+}
 
 
 @app.get('/')
@@ -10,3 +17,13 @@ def index_page():
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
     return Response(login_page, media_type='text/html')
+
+
+@app.post('/login')
+def process_login_page(username: str = Form(...), password: str = Form(...)):
+    user = users.get(username)
+    if not user or user['password'] != password:
+        return Response('Я вас не знаю!', media_type='text/html')
+    return Response(
+        f"Ваш логин: {username}<br />баланс: {user['balance']} руб.", media_type='text/html'
+    )
