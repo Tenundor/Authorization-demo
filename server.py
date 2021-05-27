41f38625a06ccde77820410e15c55fb6c2f0450f
antonz <tenundor@gmail.com>
2021-05-27 13:50:38 +0300

    Set username cookie

--- server.py
+++ server.py
@@ -24,6 +24,8 @@ def process_login_page(username: str = Form(...), password: str = Form(...)):
     user = users.get(username)
     if not user or user['password'] != password:
         return Response('Я вас не знаю!', media_type='text/html')
-    return Response(
+    response = Response(
         f"Ваш логин: {username}<br />баланс: {user['balance']} руб.", media_type='text/html'
     )
+    response.set_cookie(key='username', value=username)
+    return response
